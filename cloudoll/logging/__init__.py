68f01fa6d20e89f0ee34af176b10e9b5bebd6bd0
chuchur <chuchur@qq.com>
2023-07-11 11:38:05 +0800

    change logging

--- cloudoll/logging/__init__.py
+++ cloudoll/logging/__init__.py
@@ -42,32 +42,33 @@ class HandleLog:
     def __init__(self):
         # cur_path = os.path.dirname(os.path.realpath(__file__))  # 当前项目路径
         # log_path = os.path.join(os.path.normpath(os.getcwd() + os.sep + os.pardir), 'logs')
-        self.__logger = logging.getLogger()
+        # self.__logger = logging.getLogger()
+        self.getLogger()
         log_path = os.path.join(os.path.normpath(os.getcwd() + os.sep), "../logs")
-        now_time = datetime.now().strftime("%Y-%m-%d")  # 当前日期格式化
+        # now_time = datetime.now().strftime("%Y-%m-%d")  # 当前日期格式化
         if not os.path.exists(log_path):
             os.mkdir(log_path)  # 若不存在logs文件夹，则自动创建
+        self.log_path = log_path
+        self.log_path_prefix = None  # 当前日期格式化
+        self.all_handle = None
+        self.error_handle = None
 
-        # 收集所有日志信息文件
-        all_log_path = os.path.join(log_path, "%s-all.log" % now_time)
-        # 收集错误日志信息文件
-        error_log_path = os.path.join(log_path, "%s-error.log" % now_time)
-
-        # set handle
-        self.__logger.setLevel(logging.INFO)  # 设置默认日志记录器记录级别
-        # all
-        self.__create_handler(all_log_path, logging.INFO)
-        # error
-        self.__create_handler(error_log_path, logging.ERROR)
         # console
-        console_handle = colorlog.StreamHandler()
-        console_fmt = colorlog.ColoredFormatter(_FORMATS["console_format"], log_colors=_COLORS)
-        console_handle.setFormatter(console_fmt)
-        console_handle.setLevel(logging.DEBUG)
-        self.__logger.addHandler(console_handle)
+        if __debug__:
+            console_handle = colorlog.StreamHandler()
+            console_fmt = colorlog.ColoredFormatter(
+                _FORMATS["console_format"], log_colors=_COLORS
+            )
+            console_handle.setFormatter(console_fmt)
+            console_handle.setLevel(logging.DEBUG)
+            self.__logger.addHandler(console_handle)
+
+    def setLevel(self, level):
+        self.__logger.setLevel(level)
 
     def getLogger(self, __file):
         self.__logger = logging.getLogger(__file)  # 创建日志记录器
+
         # self._set_handle()
         return self.__logger
 
@@ -78,25 +79,44 @@ class HandleLog:
             backupCount=_LOG_FILES_COUNT,
             encoding="utf-8",
         )
-        formatter = logging.Formatter(_FORMATS["log_format"], datefmt="%Y-%m-%d %H:%M-%S")
+        formatter = logging.Formatter(
+            _FORMATS["log_format"], datefmt="%Y-%m-%d %H:%M-%S"
+        )
         handler.setFormatter(formatter)
         handler.setLevel(level=level)
         self.__logger.addHandler(handler)
         return handler
 
+    def __free(self):
+        if self.all_handle:
+            self.__logger.removeHandler(self.all_handle)
+            self.all_handle.close()
+        if self.error_handle:
+            self.__logger.removeHandler(self.error_handle)
+            self.error_handle.close()
+
     def log(self, method):
         logger = self.__logger
+        log_path_prefix = datetime.now().strftime("%Y-%m-%d")  # 当前日期格式化
+        if self.log_path_prefix != log_path_prefix:
+            self.log_path_prefix = log_path_prefix
+            self.__free()
+            # 收集所有日志信息文件
+            all_log_path = os.path.join(self.log_path, "%s-all.log" % log_path_prefix)
+            # 收集错误日志信息文件
+            error_log_path = os.path.join(
+                self.log_path, "%s-error.log" % log_path_prefix
+            )
+
+            # set handle
+            # self.__logger.setLevel(logging.INFO)  # 设置默认日志记录器记录级别
+            # all
+            self.all_handle = self.__create_handler(all_log_path, logging.INFO)
+            # error
+            self.error_handle = self.__create_handler(error_log_path, logging.ERROR)
 
         log = getattr(logger, method, None)
-        # log(message)
         return log
-        # remove
-        # logger.removeHandler(all_handler)
-        # logger.removeHandler(error_handler)
-        # logger.removeHandler(console_handle)
-        # all_handler.close()
-        # error_handler.close()
-        # console_handle.close()
 
 
 _handler = None
@@ -107,6 +127,10 @@ def _get_handle():
     handler = _handler if _handler else HandleLog()
     _handler = handler
     return handler
+
+
+def setLevel(level: int):
+    _get_handle().setLevel(level)
 
 
 def getLogger(name=None):
