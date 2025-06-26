9a74e78381a620494a7d68225fad2018670ab09a
chuchur <chuchur@qq.com>
2025-06-26 17:47:22 +0800

    fix: template demo

--- cloudoll/template/controllers/api/index.py
+++ cloudoll/template/controllers/api/index.py
@@ -47,22 +47,3 @@ async def login(ctx):
         }
     return render_error("login failed", status=401)
 
-
-@post("/api/upload", sa_ignore=True)
-async def upload(ctx, file):
-    """
-    upload api
-    """
-    file_name = file.filename  # Get the file name
-    file_content = await file.read()  # Get the file
-    file_dir = Path('static')/'upload'  # Define the upload directory
-    file_dir.mkdir(parents=True, exist_ok=True)  # Create the directory if it
-    save_path = file_dir / file_name
-    with open(save_path, "wb") as f:
-        f.write(file_content)
-
-    return {
-        "message": "upload success",
-        "file_name": file_name,
-        "file_size": len(file_content),
-    }
--- cloudoll/template/controllers/upload/upload.py
+++ cloudoll/template/controllers/upload/upload.py
@@ -1,19 +1,22 @@
 import asyncio
-from cloudoll.web import get, WebStream
+from cloudoll.web import post
+from pathlib import Path
 
+@post("/api/upload", sa_ignore=True)
+async def upload(ctx, file):
+    """
+    upload api
+    """
+    file_name = file.filename  # Get the file name
+    file_content = await file.read()  # Get the file
+    file_dir = Path("static") / "upload"  # Define the upload directory
+    file_dir.mkdir(parents=True, exist_ok=True)  # Create the directory if it
+    save_path = file_dir / file_name
+    with open(save_path, "wb") as f:
+        f.write(file_content)
 
-@get("/upload", sa_ignore=True)
-async def es(ctx):
-    ev = await WebStream(
-        ctx,
-        headers={
-            "Content-Type": "text/event-stream",
-            "Cache-Control": "no-cache",
-            "Connection": "keep-alive",
-        },
-    )
-
-    for x in range(10):
-        await ev.send(f"data: {x}\n\n")
-        await asyncio.sleep(1)
-    return ev
+    return {
+        "message": "upload success",
+        "file_name": file_name,
+        "file_size": len(file_content),
+    }
