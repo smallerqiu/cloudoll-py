c3ca46713c7d4aef74f405ef09ccfeac73532453
smallerqiu <smallerqiu@gmail.com>
2025-09-19 15:04:40 +0800

    fix: aws pg row_factory

--- cloudoll/orm/awspostgres.py
+++ cloudoll/orm/awspostgres.py
@@ -7,7 +7,8 @@ from aws_advanced_python_wrapper.sql_alchemy_connection_provider import (
 
 from cloudoll.orm.base import MeteBase, QueryTypes
 from cloudoll.logging import info, error
-from psycopg2.extras import RealDictCursor
+# from psycopg2.extras import RealDictCursor
+from psycopg.rows import dict_row
 
 
 class AwsPostgres(MeteBase):
@@ -34,7 +35,7 @@ class AwsPostgres(MeteBase):
             with AwsWrapperConnection.connect(
                 psycopg.Connection.connect, self._dsn, **self._params
             ) as conn:
-                with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                with conn.cursor(row_factory=dict_row) as cursor:
                     if (
                         query_type == QueryTypes.CREATEBATCH
                         or query_type == QueryTypes.UPDATEBATCH
