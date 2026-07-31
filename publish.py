1764e57b53403735844d09cf261065ad3ab97e74
chuchur <chuchur@qq.com>
2026-07-31 15:33:56 +0800

    fix: build

--- publish.py
+++ publish.py
@@ -3,24 +3,27 @@ import os
 import shutil
 import subprocess
 
+
 def run(cmd):
     subprocess.run(cmd, shell=True, check=True)
+
 
 def publish():
     # clean
     if os.path.exists("dist"):
         shutil.rmtree("dist")
-    
+
     # build
-    run("python -m build")
-    
+    run("python -m build --no-isolation")
+
     # upload
     run("twine upload dist/*")
-    
+
     # tag
     version = __import__("cloudoll").__version__
     run(f"git tag v{version}")
     run("git push --tags")
+
 
 if __name__ == "__main__":
     publish()
